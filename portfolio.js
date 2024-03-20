bfb7f7681a5e60abc9026382b5164c4d91955ebb
MuhammadHaider5 <151028473+MuhammadHaider5@users.noreply.github.com>
2024-03-20 12:07:49 +0500

    portfolio

---
+++ portfolio.js
@@ -0,0 +1,9 @@
+function openTab() {
+    window.open("https://www.linkedin.com/in/muhammad-haider-a14774205/", "_blank");
+}
+function openTab1() {
+    window.open("https://github.com/MuhammadHaider5", "_blank");
+}
+function openTab2() {
+    window.open("https://drive.google.com/file/d/1DAJkpSjWySAG5cQtPA6FRQYrh84xYT_S/view?usp=sharing", "_blank");
+}
